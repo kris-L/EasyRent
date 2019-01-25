10d61c2eebe3c49c2773aeea73f0684d67325605
kris <285481204@qq.com>
2019-01-25 19:11:44 +0800

    1.修改页面

--- app/src/main/java/com/rent/kris/easyrent/ui/LoginActivity.java
+++ app/src/main/java/com/rent/kris/easyrent/ui/LoginActivity.java
@@ -3,11 +3,15 @@ package com.rent.kris.easyrent.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.rent.kris.easyrent.R;
 import com.rent.kris.easyrent.ui.base.BaseActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -17,7 +21,13 @@ import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
 
+    @BindView(R.id.phone_et)
+    EditText phone_et;
+    @BindView(R.id.password_et)
+    EditText password_et;
+
     private Context mContext;
+
 
     public static void intentTo(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
@@ -42,7 +52,16 @@ public class LoginActivity extends BaseActivity {
     public void OnViewClick(View view){
         switch (view.getId()){
             case R.id.login_tv:
-                MainActivity.intentTo(LoginActivity.this);
+                String phone = phone_et.getText().toString().trim();
+                String password = password_et.getText().toString().trim();
+                if(TextUtils.isEmpty(phone)){
+                    Toast.makeText(this, "请输入手机号码", Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(password)){
+                    Toast.makeText(this, "请输入密码", Toast.LENGTH_SHORT).show();
+                }else{
+                    MainActivity.intentTo(LoginActivity.this);
+                    finish();
+                }
                 break;
             case R.id.new_user_tv:
                 PhoneBindingActivity.intentTo(LoginActivity.this);
--- app/src/main/java/com/rent/kris/easyrent/ui/PhoneBindingActivity.java
+++ app/src/main/java/com/rent/kris/easyrent/ui/PhoneBindingActivity.java
@@ -2,12 +2,22 @@ package com.rent.kris.easyrent.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rent.kris.easyrent.R;
 import com.rent.kris.easyrent.ui.base.BaseActivity;
+import com.xw.lib.custom.view.BannerView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -17,6 +27,18 @@ import butterknife.OnClick;
 
 public class PhoneBindingActivity extends BaseActivity {
 
+    @BindView(R.id.banner)
+    BannerView banner;
+    @BindView(R.id.phone_et)
+    EditText phone_et;
+
+    @BindView(R.id.next_tv)
+    TextView next_tv;
+    @BindView(R.id.get_verification_code_tv)
+    TextView get_verification_code_tv;
+
+    private boolean isCountDown = false ;
+    private TimeCount timeCount;
     private Context mContext;
     public static void intentTo(Context context) {
         Intent intent = new Intent(context, PhoneBindingActivity.class);
@@ -33,15 +55,91 @@ public class PhoneBindingActivity extends BaseActivity {
     }
 
     private void initViews() {
-
+        phone_et.addTextChangedListener(watcher);
+        banner.setRightBtn("跳过", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetPasswordActivity.intentTo(mContext);
+            }
+        });
+        banner.setRightBtnColor(getResources().getColor(R.color.black));
     }
 
-    @OnClick({R.id.next_tv})
+
+    private TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String search = editable.toString();
+            if (!TextUtils.isEmpty(search)) {
+                next_tv.setBackground(getResources().getDrawable(R.drawable.shape_circular_bead_orange));
+                if(!isCountDown){
+                    get_verification_code_tv.setBackgroundColor(getResources().getColor(R.color.green_46DECE));
+                }
+            } else {
+                next_tv.setBackground(getResources().getDrawable(R.drawable.shape_disable_btn_orange));
+                if(!isCountDown){
+                    get_verification_code_tv.setBackgroundColor(getResources().getColor(R.color.gray_D2));
+                }
+            }
+        }
+    };
+
+
+    @OnClick({R.id.next_tv,R.id.get_verification_code_tv})
     public void OnClickView(View view) {
         switch(view.getId()){
             case R.id.next_tv:
-                SetPasswordActivity.intentTo(this);
+                String phone = phone_et.getText().toString().trim();
+                if(TextUtils.isEmpty(phone)){
+                    Toast.makeText(this, "请输入手机号码", Toast.LENGTH_SHORT).show();
+                }else{
+                    SetPasswordActivity.intentTo(this);
+                }
                 break;
+
+            case R.id.get_verification_code_tv:
+                if(timeCount == null){
+                    timeCount = new TimeCount(60000, 1000);
+                    timeCount.start();
+                }else {
+                    timeCount.start();
+                }
+                break;
+        }
+    }
+
+
+    class TimeCount extends CountDownTimer {
+
+        public TimeCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            get_verification_code_tv.setBackgroundColor(getResources().getColor(R.color.gray_D2));
+            get_verification_code_tv.setClickable(false);
+            get_verification_code_tv.setText("获取验证码"+millisUntilFinished / 1000);
+            isCountDown = true;
+        }
+
+        @Override
+        public void onFinish() {
+            isCountDown = false;
+            get_verification_code_tv.setText("重新获取");
+            get_verification_code_tv.setClickable(true);
+            get_verification_code_tv.setBackgroundColor(getResources().getColor(R.color.green_46DECE));
+
         }
     }
 
