5e40dc5860f0e8fecc5755402e2ebc810943d22f
kris <285481204@qq.com>
2019-03-21 20:46:52 +0800

    1.添加权限 2.屏蔽第三方登录

--- app/src/main/java/com/rent/kris/easyrent/MyApplication.java
+++ app/src/main/java/com/rent/kris/easyrent/MyApplication.java
@@ -41,12 +41,12 @@ public class MyApplication extends MultiDexApplication {
                 ,"umeng", UMConfigure.DEVICE_TYPE_PHONE,"");
 
         //各个平台的配置，建议放在全局Application或者程序入口
-        //微信
-        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
-        //新浪微博
-        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad","http://sns.whalecloud.com");
-        //QQ登录
-        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+//        //微信
+//        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+//        //新浪微博
+//        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad","http://sns.whalecloud.com");
+//        //QQ登录
+//        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
 
         initImageLoader(this);
     }
